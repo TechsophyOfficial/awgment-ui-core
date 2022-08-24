78864030d64e07acb4b00728c64c652a081bba56
raghul.m@techsophy.com <raghul.m@techsophy.com>
2022-08-24 13:01:42 +0530

    added changes in carco config to enable file accessing outside the scr folder

--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,13 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'formiojs/dist/formio.full.min.css';
+import configFile from '../public/config.json'
 
-fetch('../public/config.json')
-    .then((r) => r.json())
-    .then((config) => {
-        ReactDOM.render(
-            <React.StrictMode>
-                <App config={config} />
-            </React.StrictMode>,
-            document.getElementById('root'),
-        );
-    });
+ReactDOM.render(
+    <React.StrictMode>
+        <App config={configFile} />
+    </React.StrictMode>,
+    document.getElementById('root'),
+)
 
     
