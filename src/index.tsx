a02e6862deda835a42b9c5f29d99b580e8531585
Gadde Vinod <vinod.g@techsophy.com>
2023-06-06 13:50:31 +0530

    Refresh change

--- src/index.tsx
+++ src/index.tsx
@@ -16,13 +16,37 @@ import 'formiojs/dist/formio.full.min.css';
 //         );
 //     });
 
-fetch(`${window.location.origin}${window.location.pathname}.json`)
-    .then(async (r) => r.json())
-    .then((config) => {
-        ReactDOM.render(
-            <React.StrictMode>
-                <App config={config} />
-            </React.StrictMode>,
-            document.getElementById('root'),
-        );
-    });
+if (sessionStorage.getItem('react-token')) {
+    let envs: any = sessionStorage.getItem('config');
+    let config: any = JSON.parse(envs);
+    console.log('i am called already logged in');
+    ReactDOM.render(
+        <React.StrictMode>
+            <App config={config} />
+        </React.StrictMode>,
+        document.getElementById('root'),
+    );
+} else {
+    console.log('i am called not logged in');
+    fetch(`${window.location.origin}${window.location.pathname}.json`)
+        .then(async (r) => r.json())
+        .then((config) => {
+            ReactDOM.render(
+                <React.StrictMode>
+                    <App config={config} />
+                </React.StrictMode>,
+                document.getElementById('root'),
+            );
+        });
+}
+
+// fetch(`${'https://awgment-mt.techsophy.com'}${window.location.pathname}.json`)
+//     .then(async (r) => r.json())
+//     .then((config) => {
+//         ReactDOM.render(
+//             <React.StrictMode>
+//                 <App config={config} />
+//             </React.StrictMode>,
+//             document.getElementById('root'),
+//         );
+//     });
