34144120383dec52fca006b85a13368122ef1cdf
Gadde Vinod <vinod.g@techsophy.com>
2023-06-14 15:51:14 +0530

    index change

--- src/index.tsx
+++ src/index.tsx
@@ -4,22 +4,11 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'formiojs/dist/formio.full.min.css';
-
-// fetch('../model/config.json')
-//     .then(async (r) => r.json())
-//     .then((config) => {
-//         ReactDOM.render(
-//             <React.StrictMode>
-//                 <App config={config} />
-//             </React.StrictMode>,
-//             document.getElementById('root'),
-//         );
-//     });
+import ErrorPage from 'components/common/ErrorPage';
 
 if (sessionStorage.getItem('react-token')) {
     let envs: any = sessionStorage.getItem('config');
     let config: any = JSON.parse(envs);
-    console.log('i am called already logged in');
     ReactDOM.render(
         <React.StrictMode>
             <App config={config} />
@@ -27,7 +16,6 @@ if (sessionStorage.getItem('react-token')) {
         document.getElementById('root'),
     );
 } else {
-    console.log('i am called not logged in');
     fetch(`${window.location.origin}${window.location.pathname}.json`)
         .then(async (r) => r.json())
         .then((config) => {
@@ -37,16 +25,18 @@ if (sessionStorage.getItem('react-token')) {
                 </React.StrictMode>,
                 document.getElementById('root'),
             );
+        })
+        .catch((error) => {
+            ReactDOM.render(<ErrorPage />, document.getElementById('root'));
+            // fetch('../config.json')
+            //     .then(async (r) => r.json())
+            //     .then((config) => {
+            //         ReactDOM.render(
+            //             <React.StrictMode>
+            //                 <App config={config} />
+            //             </React.StrictMode>,
+            //             document.getElementById('root'),
+            //         );
+            //     });
         });
 }
-
-// fetch(`${'https://awgment-mt.techsophy.com'}${window.location.pathname}.json`)
-//     .then(async (r) => r.json())
-//     .then((config) => {
-//         ReactDOM.render(
-//             <React.StrictMode>
-//                 <App config={config} />
-//             </React.StrictMode>,
-//             document.getElementById('root'),
-//         );
-//     });
